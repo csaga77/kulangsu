63fb642d2e7c09c79dd4cea752f6573c1aed096b
Mars <mars.bin.chen@gmail.com>
2025-12-22 16:06:04 +0800

    Added backpack and head

--- character.gd
+++ character.gd
@@ -22,57 +22,46 @@ extends CharacterBody2D
 		is_running = new_is_running
 		_update_state() 
 
-@onready var m_body    = $body
-@onready var m_pants   = $body/pants
-@onready var m_clothes = $body/clothes
-@onready var m_hair    = $body/hair
-@onready var m_foot    = $body/foot
+var m_body_frames     :SpriteFrames
+var m_head_frames     :SpriteFrames
+var m_clothes_frames  :SpriteFrames
+var m_pants_frames    :SpriteFrames
+var m_hair_frames     :SpriteFrames
+var m_foot_frames     :SpriteFrames
+#var m_backpack_frames :SpriteFrames
 
-var m_clothes_frames :SpriteFrames
-var m_pants_frames   :SpriteFrames
-var m_hair_frames    :SpriteFrames
-var m_foot_frames    :SpriteFrames
+var m_texture_list := {
+	"body": "res://sprites/male_body.png",
+	"foot": "res://sprites/basic_shoes_black.png",
+	"pants": "res://sprites/male_longpants.png",
+	"clothes": "res://sprites/male_longsleeve.png",
+	"backpack": "res://sprites/backpack_black.png",
+	"head": "res://sprites/male_head.png",
+	"hair": "res://sprites/hair_black.png",
+}
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	m_clothes_frames = load("res://sprites/male_animations.tres").duplicate()
-	m_pants_frames   = load("res://sprites/male_animations.tres").duplicate()
-	m_hair_frames    = load("res://sprites/male_animations.tres").duplicate()
-	m_foot_frames    = load("res://sprites/male_animations.tres").duplicate()
+	var sprite_frames_template :SpriteFrames = load("res://sprites/male_animations.tres")
+	var animated_nodes = find_children("", "AnimatedSprite2D")
+	
+	for node in animated_nodes:
+		var texture_file = m_texture_list.get(node.name)
+		if texture_file == null:
+			continue
+		var frame_texture = load(texture_file)
+		var sprite_frames :SpriteFrames = sprite_frames_template.duplicate()
+		node.sprite_frames = sprite_frames
+		var animations = sprite_frames.get_animation_names()
+		for animation_name in animations:
+			var count = sprite_frames.get_frame_count(animation_name)
+			for i in count:
+				var tex :AtlasTexture = sprite_frames.get_frame_texture(animation_name, i).duplicate()
+				if tex != null:
+					tex.atlas = frame_texture
+					sprite_frames.set_frame(animation_name, i, tex)
 
-	var cloth_texture :Texture2D = load("res://sprites/male_longsleeve.png")
-	var pant_texture  :Texture2D = load("res://sprites/male_longpants.png")
-	var hair_texture  :Texture2D = load("res://sprites/hair_black.png")
-	var foot_texture  :Texture2D = load("res://sprites/basic_shoes_black.png")
-
-	var animations = m_clothes_frames.get_animation_names()
-	for animation_name in animations:
-		var count = m_clothes_frames.get_frame_count(animation_name)
-		for i in count:
-			var tex :AtlasTexture = m_clothes_frames.get_frame_texture(animation_name, i).duplicate()
-			if tex != null:
-				tex.atlas = cloth_texture
-				m_clothes_frames.set_frame(animation_name, i, tex)
-
-			tex = m_pants_frames.get_frame_texture(animation_name, i).duplicate()
-			if tex != null:
-				tex.atlas = pant_texture
-				m_pants_frames.set_frame(animation_name, i, tex)
-
-			tex = m_hair_frames.get_frame_texture(animation_name, i).duplicate()
-			if tex != null:
-				tex.atlas = hair_texture
-				m_hair_frames.set_frame(animation_name, i, tex)
-
-			tex = m_foot_frames.get_frame_texture(animation_name, i).duplicate()
-			if tex != null:
-				tex.atlas = foot_texture
-				m_foot_frames.set_frame(animation_name, i, tex)
-
-	m_clothes.sprite_frames = m_clothes_frames
-	m_pants.sprite_frames   = m_pants_frames
-	m_hair.sprite_frames    = m_hair_frames
-	m_foot.sprite_frames    = m_foot_frames
 
 	_update_state()
 
@@ -86,8 +75,6 @@ func _normalize_angle(degrees):
 	return d
 
 func _update_state() -> void:
-	if m_body == null:
-		return
 	var animation_name = "walk" if is_walking else "idle"
 	if is_walking:
 		if is_running:
@@ -102,20 +89,11 @@ func _update_state() -> void:
 		animation_name += "left"
 	elif _is_in_range(normalized_direction, 225, 315):
 		animation_name += "down"
-	m_body.stop()
-	m_body.play(animation_name)
-	
-	m_pants.stop()
-	m_pants.play(animation_name)
-	
-	m_clothes.stop()
-	m_clothes.play(animation_name)
-	
-	m_hair.stop()
-	m_hair.play(animation_name)
-	
-	m_foot.stop()
-	m_foot.play(animation_name)
+		
+	var animated_nodes = find_children("", "AnimatedSprite2D")
+	for node in animated_nodes:
+		node.stop()
+		node.play(animation_name)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
