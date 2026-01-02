cda036fb548b5d91f369c4d7eb3f0a454dc9ecb5
Mars <mars.bin.chen@gmail.com>
2026-01-02 18:49:03 +1000

    Rotate ferry direction.

--- ferry.gd
+++ ferry.gd
@@ -34,8 +34,10 @@ func set_trans_rect(rect):
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
-
+	if Engine.is_editor_hint():
+		m_wall_mask.visible = true
+	else:
+		m_wall_mask.visible = false
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
