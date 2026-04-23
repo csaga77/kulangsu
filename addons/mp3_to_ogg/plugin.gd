60448064514f1496c2e0cb0ec22d4dff10539ded
Mars <mars.bin.chen@gmail.com>
2026-04-24 05:55:02 +1000

    Use EditorDock for MP3 to OGG plugin

--- addons/mp3_to_ogg/plugin.gd
+++ addons/mp3_to_ogg/plugin.gd
@@ -1,7 +1,10 @@
 @tool
 extends EditorPlugin
 
+const _DOCK_SLOT := EditorDock.DOCK_SLOT_RIGHT_UL
+
 var _dock: Control
+var _editor_dock: EditorDock
 
 
 func _enter_tree() -> void:
@@ -10,11 +13,21 @@ func _enter_tree() -> void:
 	if _dock.has_method("setup"):
 		_dock.setup(get_editor_interface())
 	_dock.name = "MP3 to OGG"
-	add_control_to_dock(DOCK_SLOT_RIGHT_UL, _dock)
+	_editor_dock = EditorDock.new()
+	_editor_dock.name = "MP3 to OGG Converter"
+	_editor_dock.title = "MP3 to OGG Converter"
+	_editor_dock.default_slot = _DOCK_SLOT
+	_editor_dock.layout_key = "mp3_to_ogg_converter"
+	_editor_dock.add_child(_dock)
+	add_dock(_editor_dock)
 
 
 func _exit_tree() -> void:
-	if _dock:
-		remove_control_from_docks(_dock)
+	if _editor_dock:
+		remove_dock(_editor_dock)
+		_editor_dock.queue_free()
+		_editor_dock = null
+		_dock = null
+	elif _dock:
 		_dock.queue_free()
 		_dock = null
